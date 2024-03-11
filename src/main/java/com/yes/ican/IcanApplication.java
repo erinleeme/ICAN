7f18b9952a76039c6b5af5067af83e7d7569c329
erinlee <gusdk1793@gmail.com>
2024-03-11 16:25:56 +0900

    project init setting

---
+++ src/main/java/com/yes/ican/IcanApplication.java
@@ -0,0 +1,13 @@
+package com.yes.ican;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IcanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IcanApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/yes/ican/IcanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yes.ican;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IcanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
